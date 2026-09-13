6fa763f58bbe73bd673005372f33837e4e7ed059
SnowyQuest <65355034+SnowyQuest@users.noreply.github.com>
2026-09-13 16:03:32 +0000

    bootstrap: order xz, bzip2, gzip before kmod

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -255,9 +255,9 @@ fi
 
 if [ $# -eq 0 ]; then
 	set -- linux-api-headers glibc binutils gcc make file patch pkgconf \
-	   zstd libarchive curl pacman pacman-mirrorlist fakeroot tar bash coreutils \
+	   zstd xz bzip2 gzip libarchive curl pacman pacman-mirrorlist fakeroot tar bash coreutils \
 	   util-linux sed gawk grep diffutils findutils which kmod \
-	   xz bzip2 gzip doas openrc dhcpcd iproute2 libnl iw \
+	   doas openrc dhcpcd iproute2 libnl iw \
 	   shadow e2fsprogs krelpin-keyring
 fi
 
